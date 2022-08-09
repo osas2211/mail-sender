400289715ae3af1b85c8e76e7a2b5d52eccc18dd
osaretin frank <osaretin.frank10@gmail.com>
2022-08-09 16:09:25 +0100

    mongoDB connected

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const connectDB = require("./db/connect")
 
 const PORT = process.env.PORT || 3000
 
 const start = async () => {
-  app.listen(PORT, () => {
-    console.log(`server listening to PORT ${PORT}`)
-  })
+  try {
+    await connectDB()
+    app.listen(PORT, () => {
+      console.log(`server listening to PORT ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error.message, "\nServer Crashed: MongoDB failed to connect")
+  }
 }
 
 start()
