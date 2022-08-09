d75861448eef36c7d63b8bddec70d322517beb95
osaretin frank <osaretin.frank10@gmail.com>
2022-08-09 15:57:23 +0100

    set up server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
 require("dotenv").config()
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+const start = async () => {
+  app.listen(PORT, () => {
+    console.log(`server listening to PORT ${PORT}`)
+  })
+}
+
+start()
